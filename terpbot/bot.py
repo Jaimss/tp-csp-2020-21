7b46cfa5eb14ad6e6bec297c3a04d3943c4995e1
Jaimss <james@harrell365.com>
2020-08-24 19:17:10 -0400

    basic bot

---
+++ terpbot/bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot('-')
+
+
+@bot.event
+async def on_ready():
+    act = discord.CustomActivity('Fuck you!')
+    await bot.change_presence(activity=act)
+
+    print('Bot Online!')
+
+
+@bot.command()
+async def say(ctx, channel, *, text):
+    await ctx.message.delete()
+
+    channel = ctx.message.channel_mentions[0]
+
+    await channel.send(
+        embed=discord.Embed(
+            color=0xED213A,
+            description=text
+        ).set_footer(text=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+    )
+
+
+bot.run('NzQ3NTg2MjEwMTY3MzkwMzI5.X0RB6Q.Y2QJDVtXD7dCiENa_HkgNC1BxTU')
