41dd3fe7bcb99a5a27aef74d359a07f3b01781be
Jaimss <james@harrell365.com>
2020-08-26 14:00:54 -0400

    d

--- terpbot/bot.py
+++ terpbot/bot.py
@@ -1,4 +1,7 @@
+import random
+
 import discord
+from random_word import RandomWords
 from discord.ext import commands
 
 bot = commands.Bot('-')
@@ -23,6 +26,42 @@ async def say(ctx: commands.Context, channel, *, text: str):
             color=0xED213A,
             description=text
         ).set_footer(text=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+    )
+
+
+@bot.command()
+async def solve(ctx: commands.Context, *, text: str):
+    try:
+        await ctx.channel.send(
+            embed=discord.Embed(
+                color=0xED213A,
+                description=f'`{text}` = `{eval(text)}`'
+            )
+        )
+    except:
+        await ctx.channel.send(
+            embed=discord.Embed(
+                color=0xED213A,
+                description="Give me a proper math equation dumbass."
+            )
+        )
+
+
+@bot.command()
+async def story(ctx: commands.Context):
+    words = RandomWords().get_random_words(limit=500)
+    s = ""
+    count = 0
+    for i in range(random.randint(45, 75)):
+        s += f'{random.choice(list(words))} '
+        count+=1
+
+    await ctx.channel.send(
+        embed=discord.Embed(
+            title="A Short Story",
+            description=s,
+            color=0xED213A
+        ).set_footer(text=f'Word Count: {count}')
     )
 
 
