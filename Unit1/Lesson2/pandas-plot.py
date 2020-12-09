600183df3271998de05be6958eaf316a675a18a8
jaimss <james@harrell365.com>
2020-12-09 12:46:18 -0500

    move

--- Unit1/Lesson2/pandas-plot.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.read_csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/college-majors/recent-grads.csv")
-pd.set_option("display.max.columns", None)
-
-plt.plot(data["Rank"], data[["P25th", "Median", "P75th"]])
-plt.show()
