1381571afaf058287b6d5049981b8ae0e161155f
jaimss <james@harrell365.com>
2020-12-09 12:52:00 -0500

    moving folders and rename

--- 114/two_numbers.py
+++
@@ -1,8 +0,0 @@
-divisible = False
-while not divisible:
-    first = int(input('Enter a number: '))
-    second = int(input('Enter a second (divisible by the first): '))
-
-    divisible = first % second == 0
-
-print(f'{first} and {second} are divisibile!')
